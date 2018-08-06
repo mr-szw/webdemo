d52a1a68b15e67fd4df1cd75fee1a51cda6b3c8a
Dawei <15554485117@163.com>
2018-08-06 23:20:58 +0800

    ES

--- src/main/java/com/test/webdemo/utils/EsClientUtils.java
+++ src/main/java/com/test/webdemo/utils/EsClientUtils.java
@@ -4,14 +4,21 @@ import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.MatchPhraseQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Iterator;
 
 
 /**
@@ -22,6 +29,10 @@ public class EsClientUtils {
     private static final String ES_SERVER_HOST = GetPropertyUtil.getPropertiesInfoString("elasticsearch.properties", "hostAddress", "192.168.111.21");
     private static final Integer ES_SERVER_PORT = GetPropertyUtil.getPropertiesInfoInt("elasticsearch.properties", "esPort", "9300");
 
+
+    /* 分词器 类型  默认分词器语句分成单个汉字 */
+    /* 可智能分词 */
+    private static final String ANALYZER_SMARTCN = "smartcn";
 
     /**
      * 创建连接
@@ -104,6 +115,98 @@ public class EsClientUtils {
     public static DeleteResponse deleteDocumen(TransportClient transportClient, String index1, String index2, String indexId) {
         return transportClient.prepareDelete(index1, index2, indexId).get();
     }
+
+
+
+
+
+
+    /* 多条件查寻 */
+    public static SearchResponse searchMulit(TransportClient transportClient, String index, String type) {
+        SearchRequestBuilder searchRequestBuilder = transportClient.prepareSearch(index).setTypes(type);
+        MatchPhraseQueryBuilder matchPhraseQueryBuilder = QueryBuilders.matchPhraseQuery("key1", "value1");
+        MatchPhraseQueryBuilder matchPhraseQueryBuilder1 = QueryBuilders.matchPhraseQuery("key2", "value2");
+        MatchPhraseQueryBuilder matchPhraseQueryBuilder2 = QueryBuilders.matchPhraseQuery("key3", "value3");
+        MatchPhraseQueryBuilder matchPhraseQueryBuilder3 = QueryBuilders.matchPhraseQuery("key4", "value4");
+
+        SearchResponse searchResponse = searchRequestBuilder.setQuery(QueryBuilders.boolQuery()
+                .must(matchPhraseQueryBuilder)   //匹配
+                .must(matchPhraseQueryBuilder1)   //匹配
+                .should(matchPhraseQueryBuilder2) //根据条件提高匹配得分
+                .filter(matchPhraseQueryBuilder3)   //过滤
+                )
+                // .highlighter(HighlightBuilder.fromXContent())
+                .setFetchSource(new String[]{"title", "price"}, null)
+                .execute()
+                .actionGet();
+
+        SearchHits searchHits = searchResponse.getHits();
+        Iterator<SearchHit> iterator = searchHits.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next().toString());
+        }
+        return searchResponse;
+    }
+
+
+    /* 分词查询*/
+    public static SearchResponse searchBySmchCn(TransportClient transportClient, String index, String type) {
+        SearchRequestBuilder searchRequestBuilder = transportClient.prepareSearch(index).setTypes(type);
+        SearchResponse searchResponse = searchRequestBuilder.setQuery(QueryBuilders
+                .matchQuery("key", "value")
+                .analyzer(ANALYZER_SMARTCN))
+                .setFetchSource(new String[]{"title", "price"}, null)
+                .execute()
+                .actionGet();
+
+        SearchHits searchHits = searchResponse.getHits();
+        Iterator<SearchHit> iterator = searchHits.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next().toString());
+        }
+        return searchResponse;
+
+    }
+
+
+
+
+    /* 多字段 */
+    public static SearchResponse searchMulitWord(TransportClient transportClient, String index, String type) {
+        SearchRequestBuilder searchRequestBuilder = transportClient.prepareSearch(index).setTypes(type);
+        SearchResponse searchResponse = searchRequestBuilder.setQuery(QueryBuilders
+                .multiMatchQuery("value", "key1", "key2")
+                .analyzer(ANALYZER_SMARTCN))
+                .setFetchSource(new String[]{"title", "price"}, null)
+                .execute()
+                .actionGet();
+
+        SearchHits searchHits = searchResponse.getHits();
+        Iterator<SearchHit> iterator = searchHits.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next().toString());
+        }
+        return searchResponse;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
