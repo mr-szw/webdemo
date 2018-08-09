d67228848b4250256b5679d6677d186e9023d12d
Dawei <15554485117@163.com>
2018-08-10 01:47:05 +0800

    Zk 监听

--- src/main/java/com/test/webdemo/curator/WatcherDemo.java
+++ src/main/java/com/test/webdemo/curator/WatcherDemo.java
@@ -4,7 +4,6 @@ import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.cache.TreeCache;
 import org.apache.curator.framework.recipes.cache.TreeCacheEvent;
-import org.apache.curator.framework.recipes.cache.TreeCacheListener;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
 
@@ -21,22 +20,26 @@ public class WatcherDemo {
     private static TreeCache treeCache;
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         //初始化 Client
-        curatorFrameworkClient = CuratorFrameworkFactory.builder().connectString("host:port")
-                .connectionTimeoutMs(3000).retryPolicy(new ExponentialBackoffRetry(10))
+        curatorFrameworkClient = CuratorFrameworkFactory.builder()
+                .connectString("host:port")    //连接地址
+                .connectionTimeoutMs(3000)      //设置超时时间
+                .retryPolicy(new ExponentialBackoffRetry(100, 9))   //设置重试机制 退避策略
                 .build();
 
         //启动Curator
         curatorFrameworkClient.start();
 
         treeCache = new TreeCache(curatorFrameworkClient, "/Tomcat");
-        treeCache.getListenable().addListener(new TreeCacheListener() {
-            @Override
-            public void childEvent(CuratorFramework curatorFramework, TreeCacheEvent treeCacheEvent) throws Exception {
-
-            }
+        treeCache.start();  //启动监听
+        treeCache.getListenable().addListener(( CuratorFramework curatorFramework, TreeCacheEvent treeCacheEvent) -> {
+            /*if(curatorFramework == TreeCacheEvent.Type.CONNECTION_LOST) {
+                System.out.println("连接丢失");
+            } else if( == TreeCacheEvent.Type.NODE_ADDED) {
+                System.out.println("新建连接");
+            }*/
         });
     }
 }
